bf3b1794084625cf434dac604284e2b72cf20479
Catgirl Rika <102428064+CatgirlRika@users.noreply.github.com>
2025-08-13 00:51:46 -0500

    Add blog RSS feed

--- public/blog/blog-header.php
+++ public/blog/blog-header.php
@@ -8,6 +8,7 @@ header("Content-Security-Policy: default-src 'self'; img-src 'self' data:; style
     <title><?= SITE_NAME ?> | Blog</title>
     <link rel="stylesheet" href="/static/css/normalize.min.css">
     <link rel="stylesheet" href="/static/css/style.min.css">
+    <?= $rssLinkTag ?? '<link rel="alternate" type="application/rss+xml" title="Blog RSS" href="/blog/rss.php">' ?>
 </head>
 
 <body>
